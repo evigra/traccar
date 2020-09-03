a0fd2a3204df8147ee8cb8e2c70d374c01bb8119
Eduardo Vizcaino <evigra@hotmail.com>
2020-09-03 12:28:12 -0500

    Update

--- traccar/models/model.py
+++ traccar/models/model.py
@@ -66,10 +66,12 @@ class positions(models.Model):
         print('=============== CREATE POSITIONS ===================',len(positions))                                
         self.env.cr.execute("UPDATE tc_positions SET read=1 WHERE read=0")        
         for position in positions:                    
+            
             self.create(position)
             vehicle_data                =vehicle_obj.browse(position["deviceid"])                       
             vehicle_data.devicetime     =position["devicetime"]
             vehicle_obj.write(vehicle_data)
+            print('=============== ==============',position["devicetime"],' ## ', position["devicetime"])                                
 
     def run_scheduler_table_lock(self):
         self.env.cr.execute("DELETE FROM databasechangeloglock")
